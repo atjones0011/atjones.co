7f806047fb79c743bd3b249bd747dd451077b9ed
Andrew Jones <ajones@Andrews-MBP.home>
2015-01-11 20:28:56 -0500

    Add Grunt. Change site layout

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['cssmin']);
+};
